f727aea03bac7963a909e4eeace1cca127b4598b
simonphoenix67 <simonphoenix67@gmail.com>
2023-03-26 17:11:10 +0100

    008

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,4 @@
-// import { FeedbackWidget } from './statistics/statistics';
-// import FeedbackStatistics from './counter/counter';
 
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       <FeedbackWidget />
-//       {/* <FeedbackStatistics good={good} neutral={neutral} bad={bad} /> */}
-//     </div>
-//   );
-// };
-
-
-// import React, { Component } from 'react';
-// import css from './statistics.module.css';
 
 import React, { Component } from 'react';
 import { FeedbackOptions } from './counter/counter';
--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,17 +1,3 @@
-// import React from 'react';
-
-
-// export const FeedbackStatistics = ({ good, neutral, bad }) => {
-//   const total = good + neutral + bad;
-//   const positivePercentage = total === 0 ? 0 : Math.round((good / total) * 100);
-
-//   return (
-//     <>
-//       <p>Total: {total}</p>
-//       <p>Positive feedback: {positivePercentage}%</p>
-//     </>
-//   );
-// };
 
 
 import React from 'react';
